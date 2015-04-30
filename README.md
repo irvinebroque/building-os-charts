c6e0b119b66e3ea9e43527c43457dfb6c392e0aa
zoopoetics <peter@luciddg.com>
2015-04-30 13:21:44 -0700

    added README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+## Synopsis
+
+Plug-and-play chart components based on d3 and React.
+
+## Before you do anything else
+
+`npm install`
+
+## Examples
+
+`npm run examples`
+
+## Tests
+
+`npm run tests`
+
+## License
+
+TBD
--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,8 @@ var webpack = require('webpack');
 module.exports = function(config) {
   config.set({
     autoWatch: true,
-  	browsers: ['PhantomJS'],
-  	files: [
+    browsers: ['PhantomJS'],
+    files: [
       // React explodes in PhantomJS without a bind polyfill
       './node_modules/phantomjs-polyfill/bind-polyfill.js',
       'tests.webpack.js'
